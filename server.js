d795ae7eafd84f291c16f639973631a6e4286b18
Artem Veremienko <temaspsxxx@gmail.com>
2021-08-17 22:34:27 +0300

    feat: add server express

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+const PORT = 3000;
+
+const createPath = (page) =>
+    path.resolve(__dirname, "ejs-views", `${page}.ejs`);
+
+app.listen(PORT, (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    const title = "Home";
+    res.render(createPath("index"), { title });
+});
+
+app.get("/contacts", (req, res) => {
+    const title = "Contacts";
+    const contacts = [
+        { name: "YouTube", link: "http://youtube.com" },
+        { name: "Twitter", link: "http://twitter.com" },
+        { name: "GitHub", link: "http://github.com" },
+    ];
+    res.render(createPath("contacts"), { contacts, title });
+});
+
+app.get("/posts/:id", (req, res) => {
+    const title = "Post";
+    res.render(createPath("post"), { title });
+});
+
+app.get("/posts", (req, res) => {
+    const title = "Posts";
+    res.render(createPath("posts"), { title });
+});
+
+app.get("/add-post", (req, res) => {
+    const title = "Add Post";
+    res.render(createPath("add-post"), { title });
+});
+
+app.use((req, res) => {
+    const title = "Error Page";
+    res.status(404).render(createPath("error"), { title });
+});
